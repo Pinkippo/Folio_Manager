0f5c18f5fb48eda233819b3cac81cb2ed1a49e23
Pinkippo <hseungwan8@gmail.com>
2023-10-23 21:47:58 +0900

    feat(spring): 스프링부트 백엔드 생성

---
+++ folio_back/src/main/java/com/spring/folio_back/FolioBackApplication.java
@@ -0,0 +1,13 @@
+package com.spring.folio_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolioBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FolioBackApplication.class, args);
+    }
+
+}
---
+++ folio_back/src/test/java/com/spring/folio_back/FolioBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.folio_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolioBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
