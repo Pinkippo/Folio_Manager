36d7f818bbc8a7b70c78cdb1129993456e92582e
Pinkippo <hseungwan8@gmail.com>
2023-10-29 22:31:32 +0900

    feat(flutter): 커스텀 버튼 위젯 생성
    - onPressed, Text를 받아 생성하는 커스텀 버튼 위젯 생성

--- folio_front/lib/app/widget/gradient_button.dart
+++ folio_front/lib/app/widget/gradient_button.dart
@@ -4,8 +4,17 @@ import 'package:folio_front/app/controller/login_controller.dart';
 import 'package:folio_front/data/model/register_request_model.dart';
 import 'package:get/get.dart';
 
-class GradientLoginButton extends StatelessWidget {
-  const GradientLoginButton({Key? key}) : super(key: key);
+class GradientButton extends StatelessWidget {
+
+  final String hintText;
+  final Function() onPressed;
+
+  const GradientButton(
+      {
+        Key? key,
+        required this.hintText,
+        required this.onPressed
+      }) : super(key: key);
 
   static const storage = FlutterSecureStorage();
 
@@ -25,63 +34,15 @@ class GradientLoginButton extends StatelessWidget {
         borderRadius: BorderRadius.circular(7),
       ),
       child: ElevatedButton(
-        onPressed: () async {
-          // TODO : 로그인 기능 구현
-          await Get.find<LoginController>().login().then((value){
-            storage.write(key: 'jwt', value: value);
-            // 다 지우고 다시 메인으로
-            Get.offAllNamed('/');
-          });
-        },
+        onPressed: onPressed,
         style: ElevatedButton.styleFrom(
           fixedSize: const Size(395, 55),
           backgroundColor: Colors.transparent,
           shadowColor: Colors.transparent,
         ),
-        child: const Text(
-          '로그인',
-          style: TextStyle(
-            fontWeight: FontWeight.w600,
-            fontSize: 17,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
-class GradientRegisterButton extends StatelessWidget {
-  const GradientRegisterButton({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: const LinearGradient(
-          colors: [
-            Color.fromRGBO(255, 227, 155, 1),
-            Color.fromRGBO(255, 227, 155, 0.8),
-            Color.fromRGBO(255, 227, 155, 0.6),
-          ],
-          begin: Alignment.bottomLeft,
-          end: Alignment.topRight,
-        ),
-        borderRadius: BorderRadius.circular(7),
-      ),
-      child: ElevatedButton(
-        onPressed: () {
-          // TODO : 회원가입 페이지 이동
-
-        },
-        style: ElevatedButton.styleFrom(
-          fixedSize: const Size(395, 55),
-          backgroundColor: Colors.transparent,
-          shadowColor: Colors.transparent,
-        ),
-        child: const Text(
-          '회원가입',
-          style: TextStyle(
+        child: Text(
+          hintText,
+          style: const TextStyle(
             fontWeight: FontWeight.w600,
             fontSize: 17,
           ),
